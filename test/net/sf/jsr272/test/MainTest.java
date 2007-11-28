28bf859c494865a5163eb63cabb24fb2a8c640a1
Eric Bréchemier <github@eric.brechemier.name>
2007-11-28 22:40:49 +0000

    test: completed main test sequence with JUnit Assertions and LOG4J log

--- test/net/sf/jsr272/test/MainTest.java
+++ test/net/sf/jsr272/test/MainTest.java
@@ -49,7 +49,9 @@ import javax.microedition.broadcast.ServiceContext;
 import javax.microedition.broadcast.ServiceContextListener;
 
 import javax.microedition.broadcast.esg.CommonMetadataSet;
+import javax.microedition.broadcast.esg.ProgramEvent;
 import javax.microedition.broadcast.esg.QueryComposer;
+import javax.microedition.broadcast.esg.QueryException;
 import javax.microedition.broadcast.esg.Service;
 import javax.microedition.broadcast.esg.ServiceGuide;
 import javax.microedition.broadcast.esg.ServiceGuideData;
@@ -58,13 +60,24 @@ import javax.microedition.broadcast.esg.ServiceGuideListener;
 import javax.microedition.broadcast.platform.PlatformProvider;
 import javax.microedition.broadcast.platform.PlatformProviderSelector;
 
-import net.sf.jsr272.test.esg.ServiceGuideUtil;
-import net.sf.jsr272.test.platform.PlatformProviderSelectorUtil;
+import junit.framework.Assert;
+
+import net.sf.jsr272.util.ServiceGuideUtil;
+import net.sf.jsr272.util.PlatformProviderSelectorUtil;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 
 public class MainTest implements ServiceGuideListener, ServiceContextListener
 {
+  public static String TEST_LOGGER = "";
+  protected static Logger _log;
+  
   public static void main(String[] args)
   {
+    _log = Logger.getLogger(TEST_LOGGER);
+    Assert.assertNotNull("Logger failed to initialize.",_log);
+    
     try
     {
       MainTest starter = new MainTest(args);
@@ -73,7 +86,7 @@ public class MainTest implements ServiceGuideListener, ServiceContextListener
     catch(Exception e)
     {
       e.printStackTrace();
-      System.err.println(e);
+      _log.error(e);
     }
   }
   
@@ -86,7 +99,7 @@ public class MainTest implements ServiceGuideListener, ServiceContextListener
   public MainTest(String[] args)
   {
     if(args.length!=2)
-      throw new IllegalArgumentException("Expected args: PlatformProviderId ServiceProviderName");
+      throw new IllegalArgumentException("Usage: PlatformProviderId ServiceProviderName");
     
     int _platformProviderId = Integer.parseInt(args[0]);
     String _serviceProviderName = args[1];
@@ -95,17 +108,28 @@ public class MainTest implements ServiceGuideListener, ServiceContextListener
     _zappingContextInitialized = false;
   }
 
-  protected void startTest();
+  protected void startTest()
   {
-    //TODO: replace log with log4j2me
-    //TODO: insert junit/j2meunit asserts
-    
+    // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
     // 1. Bootstrap - Select the Operator or "Platform Provider"
+    // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
     
     // 1.1 Start discovery for Platform Providers, usually scanning UHF,VHF frequency bands
     PlatformProviderSelector.discover();
+    
     // 1.2 Get the list of Platform Providers (e.g. as announced in ESG Provider List)
     PlatformProvider[] availableProvider = PlatformProviderSelector.getAvailableProviders();
+    Assert.assertNotNull
+      (
+      "getAvailableProviders() must not return null.",
+      availableProvider
+      );
+    Assert.assertTrue
+      (
+      "No platform provider found. Cannot continue the tests.",
+      availableProvider.length>0
+      );
+    
     // 1.3 Iterate over Providers and select the one with expected id
     //     an alternative would be to ask the user to choose her Operator
     boolean providerFound = false;
@@ -117,13 +141,13 @@ public class MainTest implements ServiceGuideListener, ServiceContextListener
         PlatformProviderSelector.select(availableProvider[i]);
       }
     }
-    assertTrue(providerFound,
-               "Platform "
-               +_platformProviderId
-               +" could not be found. Choose one of ["
-               +PlatformProviderSelectorUtil.getAvailableProviderListDescription()
-               +"]"
-              );
+    Assert.assertTrue
+      (
+        "Platform "+_platformProviderId+" could not be found. Choose one of ["
+        +PlatformProviderSelectorUtil.getAvailableProviderListDescription()+"]"
+        ,
+        providerFound
+      );
     
     // 1.4 Select the Electronic Service Guide corresponding to my operator.
     //     In multiple operator deployments with a common broadcaster,
@@ -140,16 +164,28 @@ public class MainTest implements ServiceGuideListener, ServiceContextListener
         _currentESG = availableESG[i];
       }
     }
-    assertNotNull(_currentESG,
-                  "Service Provider "
-                  +_serviceProviderName
-                  +" could not be found. Choose one of ["
-                  +ServiceGuideUtil.getAvailableESGListDescription()
-                  +"]"
-                 );
+    Assert.assertNotNull
+      (
+        "Service Provider "+_serviceProviderName+" could not be found. Choose one of ["
+        +ServiceGuideUtil.getAvailableESGListDescription()+"]"
+        ,
+        _currentESG
+      );
     
+    // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
     // 2. Acquire/Update ESG metadata
-    _currentESG.addListener(this, QueryComposer.currentProgram() );
+    // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+    
+    try
+    {
+      _currentESG.addListener(this, QueryComposer.currentProgram() );
+    }
+    catch(QueryException qe)
+    {
+      qe.printStackTrace();
+      Assert.fail( "currentProgram() incorrectly rejected as invalid filter: "+qe.getMessage() );
+    }
+      
     // asynchronous call
     // completion events will be received through ServiceGuideListener interface
     _currentESG.forceUpdate();
@@ -165,17 +201,35 @@ public class MainTest implements ServiceGuideListener, ServiceContextListener
     else if( ServiceGuideListener.SERVICE_GUIDE_UPDATE_FAILED.equals(event) )
     {
       _currentESG.removeListener(this);
-      fail("ESG Update failed. "+eventData);
+      Assert.fail("ESG Update failed. "+eventData);
     }
   }
   
   protected void updateCompleted()
   {
-    // 3. Query the ESG for the Channel List
-    Service[] channel = _currentESG.findServices(null);
-    assertTrue(channel.length >= 1,"No channel description found in current ESG.");
+    // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+    // 3. Query the ESG 
+    // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
     
-    // 3.1 Iterate over Channels to display the list of Channel Information
+    // 3.1 Query the ESG for the Channel List
+    Service[] channel = null;
+    try
+    {
+      channel = _currentESG.findServices(null);
+    }
+    catch(QueryException qe)
+    {
+      qe.printStackTrace();
+      Assert.fail( "null incorrectly rejected as invalid query: "+qe.getMessage() );
+    }
+    
+    Assert.assertTrue
+      (
+        "No channel description found in current ESG.",
+        channel.length >= 1
+      );
+    
+    // 3.2 Iterate over Channels to display the list of Channel Information
     System.out.println("Channel List");
     for (int i=0; i<channel.length; i++)
     {
@@ -184,12 +238,26 @@ public class MainTest implements ServiceGuideListener, ServiceContextListener
       System.out.println(i+": "+channelName+" - "+description);
     }
 
-    // 3.2 Query the ESG for current programs
-    ProgramEvent[] currentProgram = _currentESG.findPrograms( QueryComposer.currentProgram() );
-    assertTrue(currentProgram.length >= 1,"No program description found in current ESG.");
+    // 3.3 Query the ESG for current programs
+    ProgramEvent[] currentProgram = null;
+    try
+    {
+      currentProgram = _currentESG.findPrograms( QueryComposer.currentProgram() );
+    }
+    catch (QueryException qe)
+    {
+      qe.printStackTrace();
+      Assert.fail( "currentProgram() incorrectly rejected as invalid query: "+qe.getMessage() );
+    }
     
-    // 3.3 Iterate over Programs to display the current program information on all channels
-    System.out.println("Current Programs");
+    Assert.assertTrue
+      (
+        "No program description found in current ESG.",
+        currentProgram.length >= 1
+      );
+    
+    // 3.4 Iterate over Programs to display the current program information on all channels
+    _log.info("Current Programs");
     for (int i=0; i<currentProgram.length; i++)
     {
       String channelName = currentProgram[i].getStringValue(CommonMetadataSet.SERVICE_NAME);
@@ -197,10 +265,10 @@ public class MainTest implements ServiceGuideListener, ServiceContextListener
       Date endTime = currentProgram[i].getDateValue(CommonMetadataSet.PROGRAM_END_TIME);
       String title = currentProgram[i].getStringValue(CommonMetadataSet.PROGRAM_NAME);
       String synopsis = currentProgram[i].getStringValue(CommonMetadataSet.PROGRAM_DESCRIPTION);
-      System.out.println(i+": "+channelName+"["+startTime+"-"+endTime+"] "+title+" - "+synopsis);
+      _log.info(i+": "+channelName+"["+startTime+"-"+endTime+"] "+title+" - "+synopsis);
     }
     
-    // 3.4 Select channel to watch, fixed here to the first TV service.
+    // 3.5 Select channel to watch, fixed here to the first TV service.
     //    An alternative would be to ask the user to choose which channel to watch ;)
     
     boolean tvServiceFound = false;
@@ -212,9 +280,16 @@ public class MainTest implements ServiceGuideListener, ServiceContextListener
         tvServiceFound = true;
       }
     }
-    assertTrue(tvServiceFound,"No TV Channel found in ESG.");
+    Assert.assertTrue
+      (
+        "No TV Channel found in ESG.",
+        tvServiceFound
+      );
     
+    // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
     // 4. Zap to selected channel
+    // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+    
     // 4.1 Initialize the Service Context for Zapping
     ServiceContext zappingContext = ServiceContext.getDefaultContext();
     zappingContext.addListener(this);
@@ -269,7 +344,7 @@ public class MainTest implements ServiceGuideListener, ServiceContextListener
   protected void contextInstanceInitialized(ServiceContext context)
   {
     // 4.2 Apply Channel Selection
-    context.select(channel[i]);
+    context.select(_selectedChannel);
   }
   
   protected void serviceSelectionInitiated(ServiceContext context)
@@ -293,8 +368,16 @@ public class MainTest implements ServiceGuideListener, ServiceContextListener
           break;
       }
     }
-    assertTrue(videoComponentFound,"No Video Component found for TV Channel.");
-    assertTrue(audioComponentFound,"No Audio Component found for TV Channel.");
+    Assert.assertTrue
+      (
+        "No Video Component found for TV Channel.",
+        videoComponentFound
+      );
+    Assert.assertTrue
+      (
+        "No Audio Component found for TV Channel.",
+        audioComponentFound
+      );
     
     // A good place to modify selected components
   }
@@ -310,20 +393,23 @@ public class MainTest implements ServiceGuideListener, ServiceContextListener
   
   protected void playersStarted(ServiceContext context)
   {
-    // 6. End the test by stopping the zappingContext
+    // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+    // 5. End the test by stopping the zappingContext
+    // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+    
     context.stop();
   }
   
   protected void contextStopped(ServiceContext context)
   {
     // Test completed
-    System.out.println("Test Completed.");
+    _log.info("Test Completed.");
   }
   
   protected void contextClosed(ServiceContext context)
   {
     // Default Instance cannot be closed
-    fail("unexpected close of zapping context.");
+    Assert.fail("unexpected close of zapping context.");
     context.removeListener(this);
   }
 }
