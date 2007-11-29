a64e94ba990566ca298c11e1a3a413acc3ce4956
Eric Bréchemier <github@eric.brechemier.name>
2007-11-29 22:18:40 +0000

    JSR 272 API Stub for Tests: Platform Selection

---
+++ test/net/sf/jsr272/stub/platform/PlatformProviderStub.java
@@ -0,0 +1,59 @@
+/* 
+==================================================================
+ JSR 272 Mobile TV API -Evaluation Pack-
+ http://sourceforge.net/projects/jsr272/
+ 
+ Copyright (c) 2007 Eric Bréchemier <jsr272@eric.brechemier.name>
+ Licensed under BSD License and/or MIT License.
+==================================================================
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                         BSD License
+                             ~~~
+             http://creativecommons.org/licenses/BSD/
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                          MIT License
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  Copyright (c) 2007 Eric Bréchemier <jsr272@eric.brechemier.name>
+  
+  Permission is hereby granted, free of charge, to any person
+  obtaining a copy of this software and associated documentation
+  files (the "Software"), to deal in the Software without
+  restriction, including without limitation the rights to use,
+  copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the
+  Software is furnished to do so, subject to the following
+  conditions:
+
+  The above copyright notice and this permission notice shall be
+  included in all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+  OTHER DEALINGS IN THE SOFTWARE.
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*/
+package net.sf.jsr272.stub.platform;
+
+import javax.microedition.broadcast.platform.PlatformProvider;
+
+public class PlatformProviderStub extends PlatformProvider
+{
+  protected PlatformProviderStub(long id, String name)
+  {
+    super(id,name);
+  }
+  
+  public static PlatformProvider newPlatformProvider(long id, String name)
+  {
+    return new PlatformProviderStub(id,name);
+  }
+  
+}
